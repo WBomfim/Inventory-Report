e590c8c3fd7c6ff19d4560c51d56742f41333b83
WBomfim <willianbomfim@hotmail.com>
2022-12-30 18:15:16 -0300

    refactor/test: Alterado o teste que verifica o método init da classe Product

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,25 +1,36 @@
 from inventory_report.inventory.product import Product
-from tests.product.mocks import MockProduct
+
+
+def mock_product():
+    return {
+        "id": 1,
+        "nome_do_produto": "Produto 1",
+        "nome_da_empresa": "Empresa 1",
+        "data_de_fabricacao": "01/01/2021",
+        "data_de_validade": "01/01/2022",
+        "numero_de_serie": "123456789",
+        "instrucoes_de_armazenamento": "Armazenar em local seco",
+    }
 
 
 def test_cria_produto():
-    mock_product = MockProduct()
+    product_Fake = mock_product()
     new_product = Product(
-        mock_product.id,
-        mock_product.nome_do_produto,
-        mock_product.nome_da_empresa,
-        mock_product.data_de_fabricacao,
-        mock_product.data_de_validade,
-        mock_product.numero_de_serie,
-        mock_product.instrucoes_de_armazenamento,
+        product_Fake['id'],
+        product_Fake['nome_do_produto'],
+        product_Fake['nome_da_empresa'],
+        product_Fake['data_de_fabricacao'],
+        product_Fake['data_de_validade'],
+        product_Fake['numero_de_serie'],
+        product_Fake['instrucoes_de_armazenamento'],
     )
 
-    assert new_product.id == mock_product.id
-    assert new_product.nome_do_produto == mock_product.nome_do_produto
-    assert new_product.nome_da_empresa == mock_product.nome_da_empresa
-    assert new_product.data_de_fabricacao == mock_product.data_de_fabricacao
-    assert new_product.data_de_validade == mock_product.data_de_validade
-    assert new_product.numero_de_serie == mock_product.numero_de_serie
+    assert new_product.id == product_Fake['id']
+    assert new_product.nome_do_produto == product_Fake['nome_do_produto']
+    assert new_product.nome_da_empresa == product_Fake['nome_da_empresa']
+    assert new_product.data_de_fabricacao == product_Fake['data_de_fabricacao']
+    assert new_product.data_de_validade == product_Fake['data_de_validade']
+    assert new_product.numero_de_serie == product_Fake['numero_de_serie']
     assert new_product.instrucoes_de_armazenamento == (
-        mock_product.instrucoes_de_armazenamento
+        product_Fake['instrucoes_de_armazenamento']
     )
