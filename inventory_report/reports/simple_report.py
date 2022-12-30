9700d01faf53a4de504b4394978dd065fcbadc3e
WBomfim <willianbomfim@hotmail.com>
2022-12-30 15:56:23 -0300

    test: Criado o teste para validar o método init da classe Product

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,3 +1,2 @@
 class SimpleReport:
     pass
-#aquiii
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,25 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+from tests.product.mocks import MockProduct
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    mock_product = MockProduct()
+    new_product = Product(
+        mock_product.id,
+        mock_product.nome_do_produto,
+        mock_product.nome_da_empresa,
+        mock_product.data_de_fabricacao,
+        mock_product.data_de_validade,
+        mock_product.numero_de_serie,
+        mock_product.instrucoes_de_armazenamento,
+    )
+
+    assert new_product.id == mock_product.id
+    assert new_product.nome_do_produto == mock_product.nome_do_produto
+    assert new_product.nome_da_empresa == mock_product.nome_da_empresa
+    assert new_product.data_de_fabricacao == mock_product.data_de_fabricacao
+    assert new_product.data_de_validade == mock_product.data_de_validade
+    assert new_product.numero_de_serie == mock_product.numero_de_serie
+    assert new_product.instrucoes_de_armazenamento == (
+        mock_product.instrucoes_de_armazenamento
+    )
