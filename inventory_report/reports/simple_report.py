6d763354de0ee6dd0245d7dcd9e05f7c39bb4c4b
WBomfim <willianbomfim@hotmail.com>
2022-12-30 22:00:20 -0300

    feat: Criado a classe Simplereport - A Classe possui o método generate que retorna um report com a data de fabricação mais antiga, a data de validade mais próxima e a empresa com mais produtos.

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,40 @@
+from datetime import date
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, products):
+        old_date = cls.__old_fabrication_date(products)
+        nearest_date = cls.__nearest_expiration_date(products)
+        company_with_more_products = cls.__company_with_more_products(products)
+
+        return (
+            f"Data de fabricação mais antiga: {old_date}\n"
+            f"Data de validade mais próxima: {nearest_date}\n"
+            f"Empresa com mais produtos: {company_with_more_products}"
+        )
+
+    def __old_fabrication_date(products):
+        fabrication_dates = [date.fromisoformat(product["data_de_fabricacao"])
+                             for product in products]
+
+        return min(fabrication_dates)
+
+    def __nearest_expiration_date(products):
+        expiration_dates = [date.fromisoformat(product["data_de_validade"])
+                            for product in products
+                            if date.fromisoformat(product["data_de_validade"])
+                            > date.today()]
+
+        return min(expiration_dates)
+
+    def __company_with_more_products(products):
+        company_products = {}
+
+        for product in products:
+            if product["nome_da_empresa"] not in company_products:
+                company_products[product["nome_da_empresa"]] = 1
+            else:
+                company_products[product["nome_da_empresa"]] += 1
+
+        return max(company_products, key=company_products.get)
