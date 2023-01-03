c254e3710137a2d71a4a732ad35cd76881b60297
WBomfim <willianbomfim@hotmail.com>
2023-01-02 21:51:25 -0300

    feat: Criado as classes CsvImporter, JsonImporter e XmlImporter - Essas classes extendem da classe importer e implementam a leitura do arquivo especifico para cada caso em questão.

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,11 @@
+import csv
+from inventory_report.importer.importer import Importer
+
+
+class CsvImporter(Importer):
+    @classmethod
+    def import_data(cls, path):
+        if path.endswith(".csv"):
+            with open(path, "r") as file:
+                return list(csv.DictReader(file))
+        raise ValueError("Arquivo inválido")
