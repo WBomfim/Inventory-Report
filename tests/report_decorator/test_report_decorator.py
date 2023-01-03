a12a51a6e0f5109f2e76d69732187233dc687302
WBomfim <willianbomfim@hotmail.com>
2023-01-02 23:29:49 -0300

    test: criado o teste para validar o relatório colorido - A classe Coloredreport utiliza o padrão Decorator para gerar os relatórios e exibir de forma colorida, o teste valida se o padrão de cores estabelecido é respeitado na geração do relatório personalizado.

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,38 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+def mock_product_list():
+    return [
+        {
+            "id": 1,
+            "nome_do_produto": "Produto 1",
+            "nome_da_empresa": "Empresa 1",
+            "data_de_fabricacao": "2021-01-01",
+            "data_de_validade": "2024-01-01",
+            "numero_de_serie": "123456789",
+            "instrucoes_de_armazenamento": "Armazenar em local seco",
+        }
+    ]
+
+
+def expected_report():
+    product_fake = mock_product_list()[0]
+    return (
+        f"\033[32m{'Data de fabricação mais antiga:'}\033[0m"
+        + f" \033[36m{product_fake['data_de_fabricacao']}\033[0m\n"
+
+        f"\033[32m{'Data de validade mais próxima:'}\033[0m"
+        + f" \033[36m{product_fake['data_de_validade']}\033[0m\n"
+
+        f"\033[32m{'Empresa com mais produtos:'}\033[0m"
+        + f" \033[31m{product_fake['nome_da_empresa']}\033[0m\n"
+
+        "Produtos estocados por empresa:\n"
+        F"- {product_fake['nome_da_empresa']}: 1\n"
+    )
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    report = ColoredReport(CompleteReport)
+    assert report.generate(mock_product_list()) == expected_report()
