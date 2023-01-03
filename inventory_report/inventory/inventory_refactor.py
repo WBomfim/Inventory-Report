ef160733a39828d9d08367bf25f0d0a8f4e48c5e
WBomfim <willianbomfim@hotmail.com>
2023-01-02 23:56:11 -0300

    feat: Criado a classe InventoryRefactor que é a reformulação da classe Inventory

---
+++ inventory_report/inventory/inventory_refactor.py
@@ -0,0 +1,18 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class InventoryRefactor():
+    def __init__(self, importer):
+        self.importer = importer
+        self.data = []
+
+    def import_data(self, path, type_report):
+        self.data = self.importer.import_data(path)
+        if type_report == "simples":
+            return SimpleReport.generate(self.data)
+
+        if type_report == "completo":
+            return CompleteReport.generate(self.data)
+
+        raise ValueError("invalid report type")
